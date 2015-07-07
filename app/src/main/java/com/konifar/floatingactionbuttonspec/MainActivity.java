9115d806a0baf7e9a47a6c954c8ae950d2918d6f
konifar <yahpeycoy0403@gmail.com>
2015-07-07 16:40:39 +0900

    Created project

---
+++ app/src/main/java/com/konifar/floatingactionbuttonspec/MainActivity.java
@@ -0,0 +1,23 @@
+package com.konifar.floatingactionbuttonspec;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        setSupportActionBar(toolbar);
+    }
+
+}
---
+++ app/src/main/java/com/konifar/floatingactionbuttonspec/MainApplication.java
@@ -0,0 +1,12 @@
+package com.konifar.floatingactionbuttonspec;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
